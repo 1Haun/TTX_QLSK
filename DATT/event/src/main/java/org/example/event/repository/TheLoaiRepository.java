138f1568ae31ff82f171d82c37c16e456e8cbd9d
Uncle Hoang <ngohoang290404@gmail.com>
2025-02-20 15:17:22 +0700

    crud the loai

---
+++ DATT/event/src/main/java/org/example/event/repository/TheLoaiRepository.java
@@ -0,0 +1,7 @@
+package org.example.event.repository;
+
+import org.example.event.entity.TheLoai;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TheLoaiRepository extends JpaRepository<TheLoai, Long> {
+}
