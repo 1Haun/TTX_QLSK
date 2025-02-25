cd2b646714486f5d6efd7a179225016404c4a782
Uncle Hoang <ngohoang290404@gmail.com>
2025-02-25 13:04:23 +0700

    udpate the loai

--- DATT/event/src/main/java/org/example/event/repository/TheLoaiRepository.java
+++ DATT/event/src/main/java/org/example/event/repository/TheLoaiRepository.java
@@ -1,7 +1,15 @@
 package org.example.event.repository;
 
+import org.example.event.entity.BoMon;
 import org.example.event.entity.TheLoai;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface TheLoaiRepository extends JpaRepository<TheLoai, Long> {
+    @Query("SELECT DISTINCT t FROM TheLoai t WHERE LOWER(t.name) LIKE LOWER(CONCAT('%', :keyword, '%'))")
+    Page<TheLoai> findTheLoaiByName(@Param("keyword") String keyword, Pageable pageable);
+
 }
