cc8d755a99e9548bf9bb386b7075f3959887d925
Uncle Hoang <ngohoang290404@gmail.com>
2025-03-23 10:11:52 +0700

    fix xung dot

--- DATT/event/src/main/java/org/example/event/entity/SinhVien.java
+++ DATT/event/src/main/java/org/example/event/entity/SinhVien.java
@@ -1,30 +1,9 @@
 package org.example.event.entity;
 
-<<<<<<< HEAD
-
 import jakarta.persistence.*;
 import lombok.*;
 
 import java.util.List;
-
-@Entity
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
-@Table(name = "sinh_vien")
-public class SinhVien {
-
-=======
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 @Entity
 @AllArgsConstructor
@@ -33,12 +12,10 @@ import lombok.Setter;
 @Setter
 @Table(name = "sinhvien")
 public class SinhVien {
->>>>>>> origin/main
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-<<<<<<< HEAD
     private String maSinhVien;
     private String hoTen;
     private String email;
@@ -48,18 +25,4 @@ public class SinhVien {
     private List<SinhVienSuKien> danhSachSuKien;
 }
 
-=======
-    @Column(name = "ten_sinh_vien", nullable = false, unique = true, length = 255)
-    private String tenSinhVien;
 
-    @Column(name = "hoc_ky", nullable = false, length = 255)
-    private String hocKy;
-
-    @Column(name = "chuyen_nganh", nullable = false, length = 255)
-    private String chuyenNganh;
-
-    @Column(name = "bo_mon", nullable = false, length = 255)
-    private String boMon;
-
-}
->>>>>>> origin/main
--- DATT/event/src/main/java/org/example/event/repository/SinhVienRepository.java
+++ DATT/event/src/main/java/org/example/event/repository/SinhVienRepository.java
@@ -1,7 +1,5 @@
 package org.example.event.repository;
 
-<<<<<<< HEAD
-
 
 import org.example.event.entity.SinhVien;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -12,17 +10,4 @@ public interface SinhVienRepository extends JpaRepository<SinhVien, Long> {
     SinhVien findByMaSinhVien(String maSinhVien);
 }
 
-=======
-import org.example.event.entity.SinhVien;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
-public interface SinhVienRepository extends JpaRepository<SinhVien, Long> {
-    @Query("SELECT DISTINCT s FROM SinhVien s WHERE LOWER(s.tenSinhVien) LIKE LOWER(CONCAT('%', :keyword, '%')) OR LOWER(s.hocKy) LIKE LOWER(CONCAT('%', :keyword, '%'))")
-    Page<SinhVien> findSinhVienByNameOrObject(@Param("keyword") String keyword, Pageable pageable);
-
-}
->>>>>>> origin/main
